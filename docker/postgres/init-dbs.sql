508cba9898bd8bb6fa9ab5b4af5738124514823a
kastroud <johnsonolaolu@gmail.com>
2025-09-08 18:15:41 +0100

    initial commit

---
+++ docker/postgres/init-dbs.sql
@@ -0,0 +1,5 @@
+-- Create application databases
+CREATE DATABASE vendor_db;
+CREATE DATABASE client_db;
+CREATE DATABASE order_db;
+CREATE DATABASE payment_db;
---
+++ libs/database/src/database.module.ts
@@ -0,0 +1,34 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        entities: [__dirname + '/../**/*.entity.{js,ts}'],
+        ssl:
+          configService.get('NODE_ENV') == 'production'
+            ? {
+                ca: configService.get('POSTGRES_CERT').replace(/\\n/g, '\n'),
+              }
+            : false,
+        synchronize: true,
+        autoLoadEntities: true,
+      }),
+    }),
+  ],
+})
+export class DatabaseModule {}
